71cf34f48d2e019e6b8dcf0f5d4e78bdcbbfed79
KASH1718 <projectkash1718@gmail.com>
2026-09-09 15:05:04 +0530

    Update KASH-REGISTRATION.py

--- KASH/pages/KASH-REGISTRATION.py
+++ KASH/pages/KASH-REGISTRATION.py
@@ -10,7 +10,7 @@ st.markdown(f"""<style>
     }}
 </style>
     """,unsafe_allow_html=True)
-mydb=mysql.connector.connect(host="localhost",user="root",password="1708")
+mydb=mysql.connector.connect(host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",user="3H2mjs7CfMervzJ.root",password="Fv4brMEy3BcvR4xa")
 mycursor=mydb.cursor()
 mycursor.execute("CREATE DATABASE IF NOT EXISTS KASH")
 mydb.commit()
